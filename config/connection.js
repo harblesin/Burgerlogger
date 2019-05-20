a4a6779985c083aa8f8fd9ee0fe5445b3d39ea49
ylech <waltermelonson@gmail.com>
2019-05-20 10:08:21 -0700

    added code for heroku database deployment

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,21 @@
 //Requires the mysql module to set up the connection to the database
 var mysql = require("mysql");
 
-//Defines connection to database
-var connection = mysql.createConnection({
+//Defines connection to database as blank variable for later
+var connection;
+
+//Checks whether connection will be to deployed db or local
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{ 
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "123",
   database: "burgers_db"
 });
+}
 
 //Connects using the mysql connect method
 connection.connect(function (err) {
